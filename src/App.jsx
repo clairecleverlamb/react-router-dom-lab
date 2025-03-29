eaa69dd76c51de50b1ba7fc0a64264b93c6dc271
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-29 14:04:20 -0700

    wip: components config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
+
+
 
 function App() {
   return (
