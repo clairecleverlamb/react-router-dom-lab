734ec6a69c936a936fced506f981424681975277
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-30 15:36:15 -0700

    added browserrouters

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,31 @@ import NavBar from './components/NavBar/NavBar';
 import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 import MailboxForm from './components/MailboxForm/MailboxForm';
 import MailboxList from './components/MailboxList/MailboxList';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 
 function App() {
-  const [mailbox, setMailbox] = useState([]);
+  const [mailboxes, setMailboxes] = useState([]);
 
   const addMailbox = (newMailbox) => {
-    newMailbox._id = mailbox.length + 1; 
-    setMailbox([...mailbox, newMailbox]);
-  }
+    setMailboxes([...mailboxes, newMailbox]);
+  };
+
   return (
-    <>
-      <h1>Mailbox</h1>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<h2>Welcome to Post Office</h2>} />
-        <Route path='/mailboxes' element={<MailboxList mailbox={mailbox} />} />
-        <Route path='/mailboxes/new' element={<MailboxForm addMailbox={addMailbox} />} />
-        <Route path='/mailboxes/:mailboxId' element={<MailboxDetails mailbox={mailbox} />} />
-        <Route path='*' element={<h2>Whoops, something wrong!</h2>} />
-      </Routes>
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <h1>Mailbox</h1>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<h1>Welcome to Post Office</h1>} />
+          <Route path='/mailboxes' element={<MailboxList mailboxes={mailboxes} />} />
+          <Route path='/mailboxes/new' element={<MailboxForm addMailbox={addMailbox} mailboxCount={mailboxes.length} />} />
+          <Route path='/mailboxes/:mailboxId' element={<MailboxDetails mailboxes={mailboxes} />} />
+          <Route path='*' element={<h2>Whoops, something wrong!</h2>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
-export default App
+export default App;
